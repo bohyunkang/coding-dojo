0047bb9fc383849b0453c634d0a81f6965fa7595
Bohyun Kang <bbohyun@kakao.com>
2022-08-15 09:40:51 +0900

    문제 풀이 완료

--- Big3프린터/java/20220815/Solution.java
+++ Big3프린터/java/20220815/Solution.java
@@ -1,5 +1,21 @@
-class Solution {
-    public int solution(int[][] sizes) {
-        int answer = 0;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("할 일1: ");
+        String todo1 = scanner.nextLine();
+
+        System.out.print("할 일2: ");
+        String todo2 = scanner.nextLine();
+
+        System.out.print("할 일3: ");
+        String todo3 = scanner.nextLine();
+
+        System.out.println("오늘의 할 일 Big3는");
+        System.out.println("1. " + todo1);
+        System.out.println("2. " + todo2);
+        System.out.println("3. " + todo3);
     }
 }
