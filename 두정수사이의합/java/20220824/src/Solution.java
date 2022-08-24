4815b519b760ddc75ef95ea8862bffad02a20e7f
Bohyun Kang <bbohyun@kakao.com>
2022-08-24 09:39:21 +0900

    불필요한 조건 제거 및 코드 정리

--- 두정수사이의합/java/20220824/src/Solution.java
+++ 두정수사이의합/java/20220824/src/Solution.java
@@ -1,10 +1,6 @@
 class Solution {
     public long solution(int a, int b) {
-        long answer = 0;
-
-        if (a != b) {
-            answer = ((long)a + b) * (Math.abs(b - a) + 1) / 2;
-        }
+        long answer = ((long)a + b) * (Math.abs(b - a) + 1) / 2;
 
         return answer;
     }
