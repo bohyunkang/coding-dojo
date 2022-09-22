d322c216b27602da5b09d5fb1e5a992c9df0fc1e
Bohyun Kang <bbohyun@kakao.com>
2022-09-22 09:42:01 +0900

    코드 수정

--- 최대공약수와최소공배수/java/20220922/src/Solution.java
+++ 최대공약수와최소공배수/java/20220922/src/Solution.java
@@ -5,9 +5,10 @@ class Solution {
         answer = new int[2];
 
         int gcd = getGcd(n, m);
+        int lcm = n * m / gcd;
 
         answer[0] = gcd;
-        answer[1] = n * m / gcd;
+        answer[1] = lcm;
 
         return answer;
     }
