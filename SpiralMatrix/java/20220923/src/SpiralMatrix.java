816437b9d480db598974567aabf0c632020ddbc9
Bohyun Kang <bbohyun@kakao.com>
2022-09-23 20:34:30 +0900

    Scanner를 활용하여 배열 입력 받기
    - Scanner를 활용하여 배열 길이를 입력 받아 나선형 배열을 반환함

--- SpiralMatrix/java/20220923/src/SpiralMatrix.java
+++ SpiralMatrix/java/20220923/src/SpiralMatrix.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class SpiralMatrix {
     public static void main(String[] args) {
-        int length = 5;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("배열의 한 변 길이: ");
+
+        int length = scanner.nextInt();
+
+        System.out.println("나선형 배열:");
 
         int[][] spiralMatrix = spiralMatrix(length);
 
