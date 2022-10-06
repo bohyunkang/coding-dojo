329bd96488c3a1f1b06fa87c1c43d7727846dbb2
Bohyun Kang <bbohyun@kakao.com>
2022-10-06 09:59:54 +0900

    줄 바꿈 처리

--- 나머지가1이되는수찾기/java/20221006/src/Solution.java
+++ 나머지가1이되는수찾기/java/20221006/src/Solution.java
@@ -7,6 +7,8 @@ public class Solution {
 
     public int computeRemainder(int n) {
         return IntStream.range(1, n)
-                .filter(i -> n % i == 1).findFirst().getAsInt();
+                .filter(i -> n % i == 1)
+                .findFirst()
+                .getAsInt();
     }
 }
