36d6d04b238bd9fe3ace349676ce17984450497f
Bohyun Kang <bbohyun@kakao.com>
2022-10-10 23:13:11 +0900

    stream을 활용해서 다시 풀어본 최소 직사각형 문제

--- 최소직사각형/java/20221010/src/Solution.java
+++ 최소직사각형/java/20221010/src/Solution.java
@@ -1,18 +1,28 @@
+import java.util.Arrays;
+
 public class Solution {
     public int solution(int[][] sizes) {
-        return getMax(sizes);
+        return getMin(sort(sizes));
     }
 
-    public int getMax(int[][] sizes) {
+    public int[][] sort(int[][] sizes) {
+        return Arrays.stream(sizes)
+                .map(size -> Arrays.stream(size).sorted().toArray())
+                .toArray(int[][]::new);
+    }
+
+    public int getMin(int[][] sortedSizes) {
         int horizontalMax = 0;
         int verticalMax = 0;
 
-        for (int[] size : sizes) {
-            int horizontal = Math.max(size[0], size[1]);
-            int vertical = Math.min(size[0], size[1]);
+        for (int[] size : sortedSizes) {
+            if (size[0] > horizontalMax) {
+                horizontalMax = size[0];
+            }
 
-            horizontalMax = Math.max(horizontalMax, horizontal);
-            verticalMax = Math.max(verticalMax, vertical);
+            if (size[1] > verticalMax) {
+                verticalMax = size[1];
+            }
         }
 
         return horizontalMax * verticalMax;
--- 최소직사각형/java/20221010/test/SolutionTest.java
+++ 최소직사각형/java/20221010/test/SolutionTest.java
@@ -5,11 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class SolutionTest {
 
     @Test
-    void getMax() {
+    void sort() {
         Solution solution = new Solution();
 
-        assertEquals(4000, solution.getMax(new int[][]{{60, 50}, {30, 70}, {60, 30}, {80, 40}}));
-        assertEquals(120, solution.getMax(new int[][]{{10, 7}, {12, 3}, {8, 15}, {14, 7}, {5, 15}}));
-        assertEquals(133, solution.getMax(new int[][]{{14, 4}, {19, 6}, {6, 16}, {18, 7}, {7, 11}}));
+        assertArrayEquals(new int[][]{{50, 60}, {30, 70}, {30, 60}, {40, 80}},
+                solution.sort(new int[][]{{60, 50}, {30, 70}, {60, 30}, {80, 40}}));
+    }
+
+    @Test
+    void getMinimum() {
+        Solution solution = new Solution();
+
+        assertEquals(4000, solution.getMin(new int[][]{{50, 60}, {30, 70}, {30, 60}, {40, 80}}));
     }
 }
