90b867c1f9d1cd6e442dc750c89bff8226337c8e
Bohyun Kang <bbohyun@kakao.com>
2022-10-29 01:29:21 +0900

    메서드 변경
    - replace -> replaceAll

--- 숫자문자열과영단어/javascript/20221028/solution.js
+++ 숫자문자열과영단어/javascript/20221028/solution.js
@@ -2,7 +2,7 @@ const NUMBERS = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 
 
 export function convertToStringNumber(s) {
   const words = s.split('');
-  return [...words.keys()].reduce((acc, i) => acc.replace(NUMBERS[i], i), s);
+  return [...words.keys()].reduce((acc, i) => acc.replaceAll(NUMBERS[i], i), s);
 }
 
 export default function solution(s) {
--- 숫자문자열과영단어/javascript/20221028/solution.test.js
+++ 숫자문자열과영단어/javascript/20221028/solution.test.js
@@ -3,6 +3,7 @@ import solution, { convertToStringNumber } from './solution';
 test('문자열 숫자를 아라비아 숫자로 대체하자', () => {
   expect(convertToStringNumber('one4seveneight')).toBe('1478');
   expect(convertToStringNumber('onetwo39')).toBe('1239');
+  expect(convertToStringNumber('onetwoone323')).toBe('121323');
 });
 
 test('최종 테스트', () => {
