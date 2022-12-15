0de2d1e9905dcf6cd5f2a5a33a3639f72b2e77f4
Bohyun Kang <bbohyun@kakao.com>
2022-12-15 09:44:45 +0900

    [#118] JadenCase 문자열 만들기 JavaScript로 문제 풀이
    - JadenCase 문자열 만들기.

---
+++ JadenCase문자열만들기/javascript/20221215/solution.js
@@ -0,0 +1,9 @@
+function solution(s) {
+  return s
+    .split(' ')
+    .map((word) => {
+      if (word === '') return;
+      return word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
+    })
+    .join(' ');
+}
