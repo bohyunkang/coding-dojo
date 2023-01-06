a876d043b9afa938812fcde851e98b7a68759bc7
Bohyun Kang <bbohyun@kakao.com>
2023-01-06 12:25:48 +0900

    변수명 수정
    - index => dictionaryIndex

---
+++ 압축/javascript/20230106/solution.js
@@ -0,0 +1,15 @@
+function initDictionary(dictionary) {
+  const character = 'A';
+
+  for (let i = 0; i < array.length; i += 1) {
+    dictionary
+  }
+}
+
+function solution(msg) {
+  const answer = [];
+  const dictionary = new Map();
+
+  index
+  return answer;
+}
