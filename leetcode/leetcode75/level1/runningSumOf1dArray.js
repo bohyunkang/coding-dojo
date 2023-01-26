9c0617023b5f109c36b1cfd258948c9912e2efcd
Bohyun Kang <bbohyun@kakao.com>
2023-01-26 18:39:02 +0900

    [#159] 1뎁스 배열 요소별 합구하기 문제 풀이

---
+++ leetcode/leetcode75/level1/runningSumOf1dArray.js
@@ -0,0 +1,15 @@
+// 첫 번째 풀이
+const runningSum = function(nums) {
+  for (let i = 1; i < nums.length; i += 1) {
+    nums[i] += nums[i - 1];
+  }
+
+  return nums;
+};
+
+// 두 번째 풀이
+const runningSum2 = function(nums) {
+  let total = 0;
+
+  return nums.map(num => num += total);
+};
