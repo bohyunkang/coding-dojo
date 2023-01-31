12027fc640cc5ed9843688adb6fe9733b622b783
Bohyun Kang <bbohyun@kakao.com>
2023-01-31 09:58:00 +0900

    [#162] 행렬의 곱셈 JavaScript로 문제 풀이
    - 행렬의 곱셈

---
+++ programmers/level2/행렬의곱셈/javascript/20230131/solution.js
@@ -0,0 +1,14 @@
+function solution(arr1, arr2) {
+  const answer = new Array(arr1.length).fill(0)
+  .map(() => new Array(arr2[0].length).fill(0));
+
+  for (let i = 0; i < arr1.length; i += 1) {
+    for (let j = 0; j < arr2[0].length; j += 1) {
+      for (let k = 0; k < arr1[0].length; k += 1) {
+        answer[i][j] += arr1[i][k] * arr2[k][j];
+      }
+    }
+  }
+
+  return answer;
+}
