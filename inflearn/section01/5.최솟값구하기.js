443fea3dfef796b85ca2daee7d13ca9c5a2804de
Bohyun Kang <bbohyun@kakao.com>
2023-11-28 11:21:26 +0900

    [#194] reduce 사용해서 문제 풀이
    - reduce를 돌려 최솟값 구하기

--- inflearn/section01/5.최솟값구하기.js
+++ inflearn/section01/5.최솟값구하기.js
@@ -1,3 +1,4 @@
+// 1. for문 사용
 function solution1(arr) {
   let answer = arr[0];
 
@@ -10,4 +11,13 @@ function solution1(arr) {
   return answer;
 }
 
-console.log(solution1([5, 3, 7, 11, 2, 15, 17]));
+console.log(solution1([5, 3, 7, 11, 2, 15, 17]), '기대하는 값: 2');
+
+// 2. reduce 사용
+function solution2(arr) {
+  return arr.reduce((prev, curr) => {
+    return prev >= curr ? curr : prev;
+  });
+}
+
+console.log(solution2([5, 3, 7, 11, 2, 15, 17]), '기대하는 값: 2');
