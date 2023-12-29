2dda00c497f454e000836e831903cbdecfbdf031
Bohyun Kang <bbohyun@kakao.com>
2023-12-29 15:59:50 +0900

    풀이 수정
    - j + 2가 아닌 j + 1이다.

--- programmers/level1/소수만들기/javascript/20231228/solution.js
+++ programmers/level1/소수만들기/javascript/20231228/solution.js
@@ -7,7 +7,7 @@ function solution(nums) {
 
   for (let i = 0; i < nums.length; i += 1) {
     for (let j = i + 1; j < nums.length; j += 1) {
-      for (let k = j + 2; k < nums.length; k += 1) {
+      for (let k = j + 1; k < nums.length; k += 1) {
         if (i === j || i === k || j === k) {
           continue;
         }
