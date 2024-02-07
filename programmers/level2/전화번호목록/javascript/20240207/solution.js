b6c86916b036b5ecb89c33315e3c9dd7de508772
Bohyun Kang <bbohyun@kakao.com>
2024-02-07 09:29:00 +0900

    [#250] 프로그래머스 코테 스터디 Level 2 Day 11 : 전화번호 목록(풀이 2,3)
    - 풀이 2 역시 효율성 파트에서 시간이 초과됨.
    - 풀이 3은 정답!

--- programmers/level2/전화번호목록/javascript/20240207/solution.js
+++ programmers/level2/전화번호목록/javascript/20240207/solution.js
@@ -1,4 +1,4 @@
-// 풀이 1: 효율성에서 시간 초과
+// 풀이 1: 효율성에서 시간 초과(효율성 3,4번 테스트케이스)
 function isPrefix(pivot, array) {
   for (let i = 0; i < array.length; i += 1) {
     if (array[i].startsWith(pivot)) {
@@ -15,6 +15,42 @@ function solution(phone_book) {
     const array = phone_book.filter((number) => number !== pivot);
 
     if (isPrefix(pivot, array)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// 풀이 2: 똑같이 3,4번 테케 통과 못함
+function solution(phone_book) {
+  for (let i = 0; i < phone_book.length; i += 1) {
+    const pivot = phone_book[i];
+
+    for (let j = 0; j < phone_book.length; j += 1) {
+      if (i === j) {
+        continue;
+      }
+
+      if (phone_book[j].startsWith(pivot)) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+/**
+ * 풀이 3: 모든 케이스 통과
+ * 1. phone_book을 정렬한다.
+ * 2. 모든 배열을 순회하며, 기준을 phone_book[i]로 잡고, i와 i + 1을 비교하여 접두어면 false, 아니면 true 반환
+ */
+function solution(phone_book) {
+  phone_book.sort();
+
+  for (let i = 0; i < phone_book.length - 1; i += 1) {
+    if (phone_book[i + 1].startsWith(phone_book[i])) {
       return false;
     }
   }
