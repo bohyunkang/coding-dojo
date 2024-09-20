b1ee3b2946952870f7ca4d535060ffe55b2961a8
Bohyun Kang <bbohyun@kakao.com>
2024-09-21 02:49:54 +0900

    RTPS 스터디 [1. 배열] Move Zeroes
    - 설명 추가

--- leetcode/problems/283_moveZeroes.js
+++ leetcode/problems/283_moveZeroes.js
@@ -2,7 +2,12 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-// 풀이 1
+/**
+ * 풀이 1
+ * 1. 배열을 순회하며 0이 아닌 요소를 찾습니다.
+ * 2. 0이 아닌 요소를 발견하면 배열의 앞쪽(nonZeroIndex 위치)으로 이동시킵니다.
+ * 3. 이 과정을 반복하여 모든 0이 아닌 요소를 앞으로 모으고, 결과적으로 0은 뒤로 밀려납니다.
+ */
 var moveZeroes = function (nums) {
   let nonZeroIndex = 0;
 
@@ -17,7 +22,7 @@ var moveZeroes = function (nums) {
   }
 };
 
-// 풀이 2: 풀이 1 개선 방법 -> temp 변수 제거
+// 풀이 2: 풀이 1 개선 방법 -> temp 변수 제거 및 구조분해할당 활용
 var moveZeroes = function (nums) {
   let nonZeroIndex = 0;
 
