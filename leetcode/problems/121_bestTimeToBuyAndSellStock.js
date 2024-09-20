2ddd85f365dd9041583498e38e363c9da2eb6b35
Bohyun Kang <bbohyun@kakao.com>
2024-09-21 02:57:10 +0900

    RTPS 스터디 [1. 배열] Best Time To Buy And Sell Stock
    - 변수명 수정 및 설명 추가

--- leetcode/problems/121_bestTimeToBuyAndSellStock.js
+++ leetcode/problems/121_bestTimeToBuyAndSellStock.js
@@ -5,24 +5,24 @@
 // 풀이 1
 var maxProfit = function (prices) {
   /**
-    1. 배열의 첫 번째 요소를 최소 가격으로 초기화한다.
+    1. 배열의 첫 번째 요소를 최소 가격(minPrice)으로 초기화한다.
     2. 배열을 순회하면서:
-      a. 현재 가격이 최소 가격보다 작으면 최소 가격을 업데이트한다.
-      b. 그렇지 않으면, 현재 가격과 최소 가격의 차이(현재 이익)를 계산한다.
-      c. 현재 이익이 지금까지의 최대 이익보다 크면 최대 이익을 업데이트한다.
+      a. 현재 요소가 최소 가격보다 작으면 최소 가격을 업데이트한다.
+      b. 그렇지 않으면, 현재 요소와 최소 가격의 차이(현재 이익 = temp)를 계산한다.
+      c. 현재 이익이 지금까지의 최대 이익(maxProfit)보다 크면 최대 이익을 업데이트한다.
     3. 최종적으로 계산된 최대 이익을 반환한다.
   */
   let maxProfit = 0;
-  let currentPrice = prices[0];
+  let minPrice = prices[0];
 
   for (let i = 1; i < prices.length; i += 1) {
-    if (prices[i] < currentPrice) {
-      currentPrice = prices[i];
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
       continue;
     }
 
-    if (prices[i] > currentPrice) {
-      const temp = prices[i] - currentPrice;
+    if (prices[i] > minPrice) {
+      const temp = prices[i] - minPrice;
 
       if (maxProfit <= temp) {
         maxProfit = temp;
@@ -33,7 +33,7 @@ var maxProfit = function (prices) {
   return maxProfit;
 };
 
-// 풀이 2: 풀이 1 리팩토링
+// 풀이 2: 풀이 1 리팩토링 -> temp 변수, if 문 제거 및 Math.max 사용
 /**
  * @param {number[]} prices
  * @return {number}
@@ -49,7 +49,6 @@ var maxProfit = function (prices) {
     }
 
     const currentProfit = prices[i] - minPrice;
-
     maxProfit = Math.max(maxProfit, currentProfit);
   }
 
