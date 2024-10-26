ee70f6257a9f8d9a321c26e79d77a5f6e5b13121
Bohyun Kang <bbohyun@kakao.com>
2024-10-26 13:56:45 +0900

    RTPS 스터디 [5. 해시테이블] 3. Climbing Stairs
    - 문제 풀이 및 접근 방법 설명 작성

--- road-to-problem-solver/1_data-structures/week5_hash_table/3_climbingStairs.js
+++ road-to-problem-solver/1_data-structures/week5_hash_table/3_climbingStairs.js
@@ -1,1 +1,46 @@
 // 문제 링크: https://leetcode.com/problems/climbing-stairs
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+// ‼️ 중요 공식: 피보나치 수열
+//  => n번째 계단에 도달하는 방법의 수 = f(n) = f(n - 1) + f(n - 2)
+
+/**
+ * 풀이 1: 해시테이블 메모이제이션 활용
+ */
+var climbStairs = function (n) {
+  // 저장을 위한 위한 해시테이블
+  const memo = {};
+
+  function climb(n) {
+    // 이미 계산된 값이 있으면 반환
+    if (n in memo) {
+      return memo[n];
+    }
+
+    // n이 2보다 작거나 같은 경우 바로 n 반환
+    if (n <= 2) {
+      return n;
+    }
+
+    // 결과 저장 후 반환
+    memo[n] = climb(n - 1) + climb(n - 2);
+    return memo[n];
+  }
+
+  return climb(n);
+};
+
+/**
+ * 풀이 2: 재귀 함수 활용 => 🚨 시간 초과;;
+ */
+var climbStairs = function (n) {
+  if (n <= 2) {
+    return n;
+  }
+
+  // 재귀 호출
+  return climbStairs(n - 1) + climbStairs(n - 2);
+};
